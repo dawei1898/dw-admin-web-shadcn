7091e667694c424330918a7862467479a28ba4c3
dawei <187533154+dawei1898@users.noreply.github.com>
2025-11-07 22:12:17 +0800

    feat：api代理

--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,21 @@ import router from "@/routers/router.tsx";
 import ThemeProvider from "@/providers/theme-provider.tsx";
 import {Toaster} from "sonner";
 import {CircleCheck, CircleX} from "lucide-react";
+import {AuthProvider} from "@/providers/auth-provider.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ThemeProvider>
-            <Toaster
-                position='top-center'
-                icons={{
-                    success: <CircleCheck size={16} className='text-green-500'/>,
-                    error: <CircleX size={16} className='text-red-500'/>,
-                }}
-            />
-            <RouterProvider router={router}/>
+            <AuthProvider>
+                <Toaster
+                    position='top-center'
+                    icons={{
+                        success: <CircleCheck size={16} className='text-green-500'/>,
+                        error: <CircleX size={16} className='text-red-500'/>,
+                    }}
+                />
+                <RouterProvider router={router}/>
+            </AuthProvider>
         </ThemeProvider>
     </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,33 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
 import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss(),
-  ],
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
+    },
+    server: {
+        // 设置本地启动的访问地址
+        host: 'localhost',   // ip地址
+        //port: env.VITE_PORT, // 端口号
+        //port: 9110, // 端口号
+        open: true,          // 启动后是否自动打开浏览器
+        // 配置代理
+        proxy: {
+            '/dwa/api': {  // 拦截规则
+                target: 'http://localhost:8020',   // 后端本地地址
+                //changeOrigin: true,              // 是否允许跨域
+                rewrite: (path) => path.replace(/^\/dwa\/api/, '')  // 重写路径，去除 /dwa/api 前缀
+            },
+
+        }
     },
 })
